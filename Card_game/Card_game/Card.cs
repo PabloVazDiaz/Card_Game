ee972685878d92397ebd13f81929d6d7488ceaaf
Anonimo-PC\Anonimo <pablo.vazquez@alumnos.fesac.es>
2018-11-20 08:24:46 +0100

    Create project and classes

---
+++ Card_game/Card_game/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_game
+{
+    class Card
+    {
+        private int number { get; set; }
+        private string state { get; set; }
+        private string type { get; set; }
+
+
+    }
+}
---
+++ Card_game/Card_game/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_game
+{
+    class Deck
+    {
+        private List<Card> cards { get; set; }
+        private int players { get; set; }
+        private string name { get; set; }
+
+    }
+}
